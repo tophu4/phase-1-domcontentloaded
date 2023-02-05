9bf3adb1a349654a574a69d338992abdd2ea272f
Christopher Dean Canlas <chrisdcanlas@gmail.com>
2023-02-04 19:41:56 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(){
+    let text = document.getElementById("text");
+    text.textContent = "This is really cool!";
+})
+
+console.log("before html")
